b33c4376d6963404dfb1c289bf35a4292187a35e
Cody Hanson <cody.hanson@flukenetworks.com>
2013-02-07 09:19:58 -0700

    added iozone invoke, without options for now. modified .gitignore to ignore some build artifacts from compiling benchmarks from source

--- runbenchmarks.py
+++ runbenchmarks.py
@@ -6,7 +6,7 @@
 #prefixed with a timestamp for this testrun
 
 import datetime
-import sys
+#import sys not sure if need this
 import os
 
 #get the current timestamp, which will identify this benchmark run
@@ -14,30 +14,25 @@ import os
 timestamp = datetime.datetime.today() #unsure if need to worry about timezones or not
 timestampFormatStr = "%m-%d-%y-%H_%M" #not sure if this is the format we want.
 
-
-
-
 #Check /proc/cpuinfo and record the output
 returncode = os.system("cat /proc/cpuinfo > results/{0}-cpuinfo.txt".format(timestamp.strftime(timestampFormatStr)))
-
-#check returncode to make sure it worked ok?
-print returncode 
+#check returncode to make sure it worked ok
+print "Return code for cat /proc/cpuinfo:{0}".format(returncode)
 
 #run IOZONE
-
-
-
-#run iperf
-
-
-
+#Still need to figure out the options for iozone, they seem kind of complex
+returncode = os.system("./benchmarks/iozone3_414/src/current/iozone > results/{0}-iozone.txt".format(timestamp.strftime(timestampFormatStr)))
+print "Return code for iozone:{0}".format(returncode)
 
 #run STREAM
+#if the stream binary is not in, ./benchmarks/stream/ you will need to build it with make.
+#stream_l precompiled binary doesnt seem to work, needs some other library
+returncode = os.system("./benchmarks/stream/stream > results/{0}-stream.txt".format(timestamp.strftime(timestampFormatStr)))
+print "Return code for stream:{0}".format(returncode)
 
-
+#run iperf
 
 #run SPECCPU2006
 
 
-
-print "hello!"
+print "Tests completed"
