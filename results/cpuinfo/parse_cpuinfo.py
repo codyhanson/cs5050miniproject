bc02098125d05978b490f08037f3805cd9723de1
Cody Hanson <cody.hanson@flukenetworks.com>
2013-02-22 16:40:25 -0700

    updated parse_cpuinfo to tell you which files are the same and which are different

--- results/cpuinfo/parse_cpuinfo.py
+++ results/cpuinfo/parse_cpuinfo.py
@@ -6,7 +6,22 @@
 import hashlib
 import glob
 
+#dictionary of all the hashs we've seen
+hashs = {}
+
 files = glob.glob("*.txt")
 for file in (files):
    handle = open(file) 
-   print "File: {0}, md5: {1}".format(file,hashlib.md5(handle.read()).hexdigest())
+   filehash = hashlib.md5(handle.read()).hexdigest()
+   if filehash in hashs:
+       hashs[filehash].append(file)  
+   else:
+       hashs[filehash] = [file] #initialize the new list
+      
+
+for key in hashs.keys():
+    print "Files with md5: {0}".format(key)
+    for filename in hashs[key]:
+        print filename
+
+print "Number of different filehashs: {0}".format(len(hashs.keys()))
